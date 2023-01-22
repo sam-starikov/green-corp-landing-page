11552da0a9498e8d7d97421ff1664a5d630beac9
sam-starikov <sam_starikov@rambler.ru>
2023-01-22 12:21:24 +0300

    Добавили плавный скролл

--- js/animations.js
+++ js/animations.js
@@ -66,3 +66,18 @@ function updateScroll() {
 }
 
 window.addEventListener('scroll', updateScroll)
+
+function addSmoothScroll(link) {
+  link.addEventListener('click', onLinkClick)
+}
+
+function onLinkClick(event) {
+  event.preventDefault()
+  document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth',
+  })
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((el) => {
+  addSmoothScroll(el)
+})
